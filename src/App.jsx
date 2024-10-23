3dcdab6e018697894bec07d2ba18a2b03d138a4a
fozlerabbi0 <fozlerabbi.me@gmail.com>
2024-10-23 13:21:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Posts from './Posts'
+
+function App() {
+  
+
+  return (
+    <>
+      <h3>Explore Core Concept</h3>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>Props</li>
+        <li>Event Handler</li>
+        <li>State</li>
+        <li>Load Data</li>
+      </ol>
+      <hr />
+      <Posts></Posts>
+      
+    </>
+  )
+}
+
+export default App
